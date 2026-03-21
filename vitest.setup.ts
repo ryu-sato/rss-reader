7d1388e888ef371a83a86031bf3d587aa09e68e0
Ryu Sato <ryu@weseek.co.jp>
2026-03-21 05:15:11 +0000

    feat: .gitignoreにSQLiteデータベースファイルを追加し、package.jsonのテストスクリプトを更新

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,19 @@
 import '@testing-library/jest-dom'
+import { beforeEach } from 'vitest';
+import { spawn } from 'child_process';
+import path from 'path';
+
+beforeEach(async() => {
+  // DBをリセットしてからテストを実行
+  const prismaBin = path.join(process.cwd(), 'node_modules', '.bin', 'prisma');
+  await new Promise((res, rej) => {
+    const process = spawn(
+      prismaBin,
+      ['migrate', 'reset', '--force'],
+      {
+          stdio: 'ignore',
+      }
+    );
+    process.on('exit', (code) => (code === 0 ? res(0) : rej(code)));
+  })
+})
